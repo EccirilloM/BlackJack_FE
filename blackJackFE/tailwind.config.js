0d83880acad75f8dff755c579752f7cd1d76ebc6
Ecirillo <ettore.cirillo.dev@gmail.com>
2024-06-22 16:16:43 +0200

    first commit

---
+++ blackJackFE/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "background":"#15202b",
+        "cardBackground":"#2d3741",
+        "cardDark":"#12161a",
+      }
+    },
+  },
+  plugins: [],
+}
+
