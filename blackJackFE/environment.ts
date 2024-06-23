67f138b0963d2f91176136c44ee581d47f7c008b
Ecirillo <ettore.cirillo.dev@gmail.com>
2024-06-23 16:56:16 +0200

    implementazione FE environment

--- blackJackFE/environment.ts
+++ blackJackFE/environment.ts
@@ -1,2 +1,2 @@
-export const globalBackendUrl: string = 'https://bjsafer-20e095b91f94.herokuapp.com/api/v1/';
+export const globalBackendUrl: string = 'https://bjsaferbe-ab7611a22aa1.herokuapp.com/api/v1/';
 export const production: boolean = true;
