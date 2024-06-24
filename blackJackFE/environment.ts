2a2b2346801dc1bf16f2ada05bbf9440a9f0994b
Ecirillo <ettore.cirillo.dev@gmail.com>
2024-06-24 20:26:44 +0200

    correzione file per localhost

--- blackJackFE/environment.ts
+++ blackJackFE/environment.ts
@@ -1,2 +1,2 @@
-export const globalBackendUrl: string = 'https://bjsaferbe-ab7611a22aa1.herokuapp.com/api/v1/';
+export const globalBackendUrl: string = 'http://localhost:8080/api/v1/';
 export const production: boolean = true;
